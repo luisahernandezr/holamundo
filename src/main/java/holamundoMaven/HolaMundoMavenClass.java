ea6c716bb9fd2526c14263c3cdf1acf4a7d4e869
LUISA <lufe.hernandez@gmail.com>
2013-10-17 11:41:04 -0300

    commit mensaje prueba luisa

---
+++ src/main/java/holamundoMaven/HolaMundoMavenClass.java
@@ -0,0 +1,15 @@
+package holamundoMaven;
+
+import javax.swing.JOptionPane;
+
+public class HolaMundoMavenClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("Hola mundo maven, ha creado su primer programa maven con java");
+		JOptionPane.showMessageDialog(null,"hola mundo maven");
+
+	}
+
+}
